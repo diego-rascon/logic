02f437b273ce832d95b2340304acdb1dc5ae0c93
Diego Rascón <joserascon2002@gmail.com>
2024-02-28 12:21:13 -0700

    Added and, or and copy method

--- Clause.py
+++ Clause.py
@@ -1,9 +1,30 @@
+from Formula import Formula
+from Atom import Atom
+
+
 class Clause:
     def __init__(self):
         self.atoms = []
 
-    def add_atom(self, new_atom):
+    def or_atom(self, new_atom):
         self.atoms.append(new_atom)
+
+    def and_atom(self, atom):
+        formula = Formula()
+        formula.and_clause(self.__copy__())
+        clause = Clause()
+        clause.or_atom(atom.__copy__())
+        formula.and_clause(clause)
+        return formula
+
+    def or_clause(self, clause):
+        for atom in clause.atoms:
+            self.atoms.append(atom.__copy__())
+
+    def __copy__(self):
+        clause_copy = Clause()
+        for atom in self.atoms:
+            clause_copy.atoms.append(atom.__copy__())
 
     def __str__(self):
         result = '('
