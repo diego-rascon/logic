968f5e20e9426b55f4c521fcd5f2de9953550da5
Diego Rascón <joserascon2002@gmail.com>
2024-02-28 14:08:44 -0700

    Clauses modified

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from Formula import Formula
 from Atom import Atom
 from Clause import Clause
 
 atom_a = Atom('A')
 atom_b = Atom('B')
 atom_b.invert()
-clause_1 = Clause()
-clause_1.or_atom(atom_a)
-clause_1.or_atom(atom_b)
+atom_c = Atom('c')
 
-formula_1 = clause_1.and_atom(atom_b)
+clause_1 = Clause()
+clause_2 = Clause()
+
+clause_1 = clause_1.or_atom(atom_a)
+clause_1 = clause_1.or_atom(atom_b)
+clause_1 = clause_1.or_atom(atom_c)
+
+formula_1 = clause_1.invert()
 
 print(formula_1)
