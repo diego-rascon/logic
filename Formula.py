e87e965318173cafa0748eb62ea65cda19bbe653
Diego Rascón <joserascon2002@gmail.com>
2024-02-28 14:07:40 -0700

    And Clause method added

--- Formula.py
+++ Formula.py
@@ -2,8 +2,15 @@ class Formula:
     def __init__(self):
         self.clauses = []
 
-    def and_clause(self, clause):
-        self.clauses.append(clause)
+    def and_clause(self, new_clause):
+        formula = Formula()
+
+        for clause in self.clauses:
+            formula.clauses.append(clause.__copy__())
+
+        formula.clauses.append(new_clause.__copy__())
+
+        return formula
 
     def __str__(self):
         result = '['
