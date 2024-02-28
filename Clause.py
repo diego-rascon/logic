53981123bf6d5a90906bfacef4de501f2badf58a
Diego Rascón <joserascon2002@gmail.com>
2024-02-28 14:08:07 -0700

    Implemented an invert method

--- Clause.py
+++ Clause.py
@@ -1,30 +1,67 @@
 from Formula import Formula
-from Atom import Atom
 
 
 class Clause:
     def __init__(self):
         self.atoms = []
 
+    def invert(self):
+        formula = Formula()
+
+        for atom in self.atoms:
+            new_atom = atom.__copy__()
+            new_atom.invert()
+            new_clause = Clause()
+            new_clause.atoms.append(new_atom)
+            formula = formula.and_clause(new_clause)
+
+        return formula
+
     def or_atom(self, new_atom):
-        self.atoms.append(new_atom)
+        new_clause = Clause()
+
+        for atom in self.atoms:
+            new_clause.atoms.append(atom.__copy__())
+
+        new_clause.atoms.append(new_atom.__copy__())
+
+        return new_clause
 
     def and_atom(self, atom):
         formula = Formula()
         formula.and_clause(self.__copy__())
+
         clause = Clause()
         clause.or_atom(atom.__copy__())
+
         formula.and_clause(clause)
+
         return formula
 
     def or_clause(self, clause):
+        new_clause = Clause()
+
+        for atom in self.atoms:
+            new_clause.atoms.append(atom.__copy__())
+
         for atom in clause.atoms:
-            self.atoms.append(atom.__copy__())
+            new_clause.atoms.append(atom.__copy__())
+
+        return new_clause
+
+    def and_clause(self, clause):
+        formula = Formula()
+        formula = formula.and_clause(self)
+        formula = formula.and_clause(clause)
+        return formula
 
     def __copy__(self):
         clause_copy = Clause()
+
         for atom in self.atoms:
             clause_copy.atoms.append(atom.__copy__())
+
+        return clause_copy
 
     def __str__(self):
         result = '('
