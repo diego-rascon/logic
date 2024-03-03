58fdcd65cd4fd0396ffddc50dcd154b4f7692ae0
Diego Rascón <joserascon2002@gmail.com>
2024-03-02 18:58:30 -0700

    Regular expression commented

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from Atom import Atom
-from Clause import Clause
+from infix_postfix import infix_to_postfix
 
-atom_a = Atom('A')
-atom_b = Atom('B')
-atom_b.invert()
-atom_c = Atom('c')
+file = open('formulas/formula_1.txt')
+rows = file.readlines()
 
-clause_1 = Clause()
-clause_2 = Clause()
 
-clause_1 = clause_1.or_atom(atom_a)
-clause_1 = clause_1.or_atom(atom_b)
-clause_1 = clause_1.or_atom(atom_c)
-
-formula_1 = clause_1.invert()
-
-print(formula_1)
+for row in rows:
+    row = row.rstrip()
+    infix_to_postfix(row)
+    '''
+    print(f'Row: {row}')
+    infix = re.findall('(\\w+|\\||\\&|\\>|\\-|\\(|\\)|\\=)', row)
+    print(f'Infix: {row}')
+    postfix = infix_to_postfix(row)
+    print(f'Postfix: {postfix}')
+    '''
