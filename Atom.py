6bbbfb9a225f07f7b480ad60ae0e6b39bd21fd01
Diego Rascón <joserascon2002@gmail.com>
2024-03-19 23:56:19 -0700

    Invert method implemented

--- Atom.py
+++ Atom.py
@@ -1,10 +1,12 @@
 class Atom:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.inverted = False
 
     def invert(self):
-        self.inverted = not self.inverted
+        atom_copy = Atom(self.name)
+        atom_copy.inverted = not atom_copy.inverted
+        return atom_copy
 
     def __copy__(self):
         atom_copy = Atom(self.name)
