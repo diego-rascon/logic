5e679a6cd9149e8550225941df3ab3ee2ca45c3a
Diego Rascón <joserascon2002@gmail.com>
2024-04-02 11:15:10 -0700

    Se dividieron las diferentes formas de crear formulas en funciones

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 import re
 
+from Atom import Atom
+from Clause import Clause
 from infix_to_postfix import infix_to_postfix, evaluate
 
-file = open("formulas/fnc/fnc_2.txt")
-lines = file.readlines()
 
-for line in lines:
-    print(f'Line: {line}')
-    infix_exp = re.findall("(\\w+|\\||&|>|-|\\(|\\)|=)", line)
-    print(f'Infijo: {infix_exp}')
+def read_file():
+    file = open("formulas/fnc.txt")
+    lines = file.readlines()
 
-    postfix_exp = infix_to_postfix(infix_exp)
-    print(f'Postfijo: {postfix_exp}')
+    for line in lines:
+        print(f'Line: {line}')
+        infix_exp = re.findall("(\\w+|\\||&|>|-|\\(|\\)|=)", line)
+        print(f'Infijo: {infix_exp}')
 
-    fnc = evaluate(postfix_exp)
-    print(f'FNC: {fnc}')
+        postfix_exp = infix_to_postfix(infix_exp)
+        print(f'Postfijo: {postfix_exp}')
+
+        fnc = evaluate(postfix_exp)
+        print(f'FNC: {fnc}')
+
+
+def new_clause():
+    c1 = Clause()
+    c1 = c1.or_atom(Atom('a'))
+    c1 = c1.or_atom(Atom('b').invert())
+    c1 = c1.or_atom(Atom('b'))
+
+    print(c1)
+    print(c1.is_tautology())
+
+
+read_file()
+# new_clause()
