ed781501aa62f87ceeb58ea816e37aeec2084263
Diego Rascón <joserascon2002@gmail.com>
2024-04-02 11:14:46 -0700

    Se corrigió el método para invertir

--- Atom.py
+++ Atom.py
@@ -1,10 +1,13 @@
 class Atom:
     def __init__(self, name: str):
-        self.name = name.capitalize()
+        self.name = name.upper()
         self.inverted = False
 
     def invert(self):
         self.inverted = not self.inverted
+        atom_copy = Atom(self.name)
+        atom_copy.inverted = not atom_copy.inverted
+        return atom_copy
 
     def __copy__(self):
         atom_copy = Atom(self.name)
