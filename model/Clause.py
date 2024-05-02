9101ff5821fd7b3edc4dd43587fbd3a5fbbdbdd7
Diego Rascón <joserascon2002@gmail.com>
2024-05-02 00:30:01 -0700

    Se renombró la lista de atomos a atoms

--- model/Clause.py
+++ model/Clause.py
@@ -4,12 +4,12 @@ from model.Formula import Formula
 
 class Clause:
     def __init__(self):
-        self.atomos = []
+        self.atoms = []
 
     def invert_clause(self):
         new_formula = Formula()
 
-        for atom in self.atomos:
+        for atom in self.atoms:
             new_clause = Clause()
             atom_copy: Atom = atom.__copy__()
             atom_copy = atom_copy.invert_atom()
@@ -21,10 +21,10 @@ class Clause:
     def or_clause(self, input_clause):
         new_formula = Clause()
 
-        for atom in self.atomos:
+        for atom in self.atoms:
             new_formula = new_formula.or_atom(atom.__copy__())
 
-        for atom in input_clause.atomos:
+        for atom in input_clause.atoms:
             new_formula = new_formula.or_atom(atom.__copy__())
 
         return new_formula
@@ -39,11 +39,11 @@ class Clause:
     def or_atom(self, input_atom):
         new_clause = Clause()
 
-        for atom in self.atomos:
+        for atom in self.atoms:
             new_clause = new_clause.or_atom(atom.__copy__())
 
         if self.compare(input_atom):
-            new_clause.atomos.append(input_atom.__copy__())
+            new_clause.atoms.append(input_atom.__copy__())
 
         return new_clause
 
@@ -59,7 +59,7 @@ class Clause:
         return new_formula
 
     def compare(self, input_atom):
-        for a in self.atomos:
+        for a in self.atoms:
             if input_atom.name == a.name:
                 if input_atom.inverted == a.inverted:
                     return False
@@ -69,10 +69,10 @@ class Clause:
     def __str__(self):
         result = "("
 
-        for index, atom in enumerate(self.atomos):
+        for index, atom in enumerate(self.atoms):
             result += atom.__str__()
 
-            if index != len(self.atomos) - 1:
+            if index != len(self.atoms) - 1:
                 result += " | "
 
         result += ")"
@@ -82,7 +82,7 @@ class Clause:
     def __copy__(self):
         clause_copy = Clause()
 
-        for atom in self.atomos:
-            clause_copy.atomos.append(atom.__copy__())
+        for atom in self.atoms:
+            clause_copy.atoms.append(atom.__copy__())
 
         return clause_copy
